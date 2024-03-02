deae4b1969beffef9f04768ecbfea60e433a14d8
Zeynep Atik <zeynepatik2003@gmail.com>
2024-03-02 12:44:56 +0300

    task.py load

---
+++ tasks.py
@@ -0,0 +1,27 @@
+# Görev1 :xlsx dosyasını okutunuz ve veri seti ile ilgili genel bilgileri gösteriniz.
+# Görev2: Kaç unique şehirr var? Frekansları nedir? 
+# Görev3 : Kaç unique Concept vardır
+# Görev4 : Hangi Concept' den kaçar tane satış gerçekleşmiş?
+# Görev5 : Şehirlere göre satışlardan toplam ne kadar kazanılmış?
+# Görev6 :  Concept türlerinegöre ne kada kazanlmış?
+# Görev7 : Şehirlere göre PRICE ortalamaları nedir?
+# Görev8 : Conceptlere göre Prıce ortalamaları nedir?
+# Görev9 : Şehir-Concept kırılımında PRICE ortalamaları nedir?
+# Görev 10: SaleCheckInDayDiff değişkeninin kategorik bir değişkene çeviriniz.
+# SaleCheckInDayDiff değişkeni müşterinin CheckIn tarihinden ne kadar önce satın alımını tamamladığını  gösterir.
+# Aralıkları ikna edici şekilde oluştur ( "0_7","7_30", "30_90","90_max")
+# Bu aralıklar için "Last Minuters", "Potential Planners", "Planners", "Early Bookers" isimlerin,i kullan.
+# Görev 11: Şehir,Concept, [EB_Score,Sezon,CInday] kırılımında ücret ortalamalarına ve frekanslarına bakınız
+# City-Concept-Season kırılımının çıktısını PRICE göre sırlayınız
+# elde ettiğin çıktıyı ag_df olarak kaydet.
+# Görev 12: Indekste yer alan isimleri değişken ismine çeviriniz.( 3 cü sorunun çıktısındaki prıce hariç hepsi indeks değerdir bunlaı değişken ismine çevir)
+# Görev 13:  Yeni level based satışları tanımlayınız ve veri setine değişken olarak ekleyiniz.
+#############################################
+# sales_level_based adında bir değişken tanımlayınız ve veri setine bu değişkeni ekleyiniz.
+# Görev 14 :  Personaları segmentlere ayırınız.
+#############################################
+# PRICE'a göre segmentlere ayırınız,
+# segmentleri "SEGMENT" isimlendirmesi ile agg_df'e ekleyiniz
+# segmentleri betimleyiniz
+# Görev 15:  Oluşan son df'i price değişkenine göre sıralayınız.
+# "ANTALYA_HERŞEY DAHIL_HIGH" hangi segmenttedir ve ne kadar ücret beklenmektedir?
